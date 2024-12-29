ba4cedeec2bb8a7abdb776773cac540ccbea0b6f
evanbacon <bacon@expo.io>
2024-12-29 13:43:53 -0600

    wip -- meh

--- components/header.tsx
+++ components/header.tsx
@@ -6,15 +6,16 @@ const bg = AC.systemBackground;
 
 export default function DotHeader({
   flip,
+  style,
 }: {
   flip?: boolean;
+  style: any;
   dom?: import("expo/dom").DOMProps;
 }) {
   return (
     <div
       style={{
         flex: 1,
-        backgroundColor: "transparent",
         backgroundImage: `radial-gradient(transparent 1.5px, ${bg} 1.5px)`,
         backgroundSize: "5px 5px",
 
@@ -23,6 +24,7 @@ export default function DotHeader({
           ? `linear-gradient(rgba(255, 255, 255, 0) 0%, ${bg} 100%)`
           : `linear-gradient(${bg} 60%, rgba(255, 255, 255, 0) 100%)`,
         opacity: 1,
+        ...style,
       }}
     />
   );
--- components/ui/Stack.tsx
+++ components/ui/Stack.tsx
@@ -1,4 +1,6 @@
 import { Stack as NativeStack } from "expo-router";
+import { View } from "react-native";
+import MaskedView from "@react-native-masked-view/masked-view";
 
 // These are the default stack options for iOS, they disable on other platforms.
 const DEFAULT_STACK_HEADER: import("@react-navigation/native-stack").NativeStackNavigationOptions =
@@ -6,8 +8,8 @@ const DEFAULT_STACK_HEADER: import("@react-navigation/native-stack").NativeStack
     ? {}
     : {
         headerTransparent: true,
-        headerBlurEffect: "systemChromeMaterial",
-        headerShadowVisible: true,
+        // headerBlurEffect: "systemChromeMaterial",
+        // headerShadowVisible: true,
         headerLargeTitleShadowVisible: false,
         headerLargeStyle: {
           backgroundColor: "transparent",
@@ -15,6 +17,8 @@ const DEFAULT_STACK_HEADER: import("@react-navigation/native-stack").NativeStack
         headerLargeTitle: false,
       };
 
+import Header from "@/components/header";
+import { BlurView } from "expo-blur";
 export default function Stack({
   screenOptions,
   ...props
@@ -24,6 +28,71 @@ export default function Stack({
       screenOptions={{
         ...DEFAULT_STACK_HEADER,
         ...screenOptions,
+        headerBackground() {
+          console.log(props);
+          // Custom header view
+          return (
+            <View
+              style={{
+                flex: 1,
+                maxHeight: 96,
+                pointerEvents: "none",
+              }}
+            >
+              <MaskedView
+                style={{
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  right: 0,
+                  bottom: 0,
+                }}
+                maskElement={
+                  <View
+                    style={{
+                      position: "absolute",
+                      top: 0,
+                      left: 0,
+                      right: 0,
+                      bottom: 0,
+                      width: "100%",
+                      height: "100%",
+                      experimental_backgroundImage: `linear-gradient(black 60%, rgba(255, 255, 255, 0) 100%)`,
+                    }}
+                  />
+                }
+              >
+                <BlurView
+                  intensity={60}
+                  tint={"default"}
+                  style={{
+                    flex: 1,
+                  }}
+                />
+              </MaskedView>
+              <Header
+                style={{
+                  height: 96,
+                }}
+                dom={{
+                  pointerEvents: "none",
+                  textInteractionEnabled: false,
+                  scrollEnabled: false,
+                  contentInsetAdjustmentBehavior: "never",
+                  matchContents: true,
+                  containerStyle: {
+                    position: "absolute",
+                    flex: 1,
+                    top: 0,
+                    left: 0,
+                    bottom: 0,
+                    right: 0,
+                  },
+                }}
+              />
+            </View>
+          );
+        },
       }}
       {...props}
     />
